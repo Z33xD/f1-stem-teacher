5cdbc1374aa308ba0a410d2992953084f13383ec
Z33xD <zeeshanmoinshariff@gmail.com>
2026-05-31 17:59:56 +0530

    Changing DEBUG mode to false, so that it is ready for production

--- project/backend/settings.py
+++ project/backend/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-yfwm_dz4y9_#%b7__ndj@4w4g-w+ddcs+a^i)psl5yp*^ficfn'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
